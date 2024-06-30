41e6810cf62dc566549c3d57a2366201a701f918
Marcio de Andrade <marcio@requestdev.net>
2024-06-30 14:25:46 -0400

    perfil de mapeamento para configurar o AutoMapper

---
+++ GerenciadorTarefasAPI/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GerenciadorTarefasAPI.DTOs;
+using GerenciadorTarefasAPI.Models;
+
+namespace GerenciadorTarefasAPI.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Projeto, ProjetoDTO>().ReverseMap();
+            CreateMap<Tarefa, TarefaDTO>().ReverseMap();
+            CreateMap<TarefaHistorico, TarefaHistoricoDTO>().ReverseMap();
+        }
+    }
+}
