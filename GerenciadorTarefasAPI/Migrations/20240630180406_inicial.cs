e39949501a4353faf133bc568a9926343485ecdc
Marcio de Andrade <marcio@requestdev.net>
2024-06-30 14:05:00 -0400

    Migrations

---
+++ GerenciadorTarefasAPI/Migrations/20240630180406_inicial.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GerenciadorTarefasAPI.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projetos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projetos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prioridade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AtualizadoEm = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProjetoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tarefas_Projetos_ProjetoId",
+                        column: x => x.ProjetoId,
+                        principalTable: "Projetos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TarefaHistoricos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TarefaId = table.Column<int>(type: "int", nullable: false),
+                    AlteraDescricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AlteraDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AlteradoPor = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TarefaHistoricos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TarefaHistoricos_Tarefas_TarefaId",
+                        column: x => x.TarefaId,
+                        principalTable: "Tarefas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TarefaHistoricos_TarefaId",
+                table: "TarefaHistoricos",
+                column: "TarefaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarefas_ProjetoId",
+                table: "Tarefas",
+                column: "ProjetoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TarefaHistoricos");
+
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+
+            migrationBuilder.DropTable(
+                name: "Projetos");
+        }
+    }
+}
--- GerenciadorTarefasAPI/Program.cs
+++ GerenciadorTarefasAPI/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using GerenciadorTarefasAPI.Data;
+using GerenciadorTarefasAPI.Repositories;
+using GerenciadorTarefasAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProjetoRepository, ProjetoRepository>();
+builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
+builder.Services.AddScoped<ProjetoService>();
+builder.Services.AddScoped<TarefaService>();
 
 var app = builder.Build();
 
