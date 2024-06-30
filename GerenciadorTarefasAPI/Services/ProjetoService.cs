7f9f6001bcfe49ba97f0ef01959f584def5c39a2
Marcio de Andrade <marcio@requestdev.net>
2024-06-30 13:50:35 -0400

    Services

---
+++ GerenciadorTarefasAPI/Services/ProjetoService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GerenciadorTarefasAPI.Models;
+using GerenciadorTarefasAPI.Repositories;
+
+namespace GerenciadorTarefasAPI.Services
+{
+    public class ProjetoService
+    {
+        private readonly IProjetoRepository _projetoRepository;
+
+        public ProjetoService(IProjetoRepository projetoRepository)
+        {
+            _projetoRepository = projetoRepository;
+        }
+
+        public async Task<IEnumerable<Projeto>> GetProjetosAsync()
+        {
+            return await _projetoRepository.GetProjetosAsync();
+        }
+
+        public async Task<Projeto> GetProjetoByIdAsync(int id)
+        {
+            return await _projetoRepository.GetProjetoByIdAsync(id);
+        }
+
+        public async Task CreateProjetoAsync(Projeto projeto)
+        {
+            await _projetoRepository.AddProjetoAsync(projeto);
+        }
+
+        public async Task RemoveProjetoAsync(int id)
+        {
+            var projeto = await _projetoRepository.GetProjetoByIdAsync(id);
+            if (projeto == null)
+            {
+                throw new InvalidOperationException("Projeto não encontrado.");
+            }
+
+            if (projeto.Tarefas.Any(t => t.Status != "Concluída"))
+            {
+                throw new InvalidOperationException("Não é possível remover um projeto com tarefas pendentes.");
+            }
+
+            await _projetoRepository.RemoveProjetoAsync(id);
+        }
+    }
+}
---
+++ GerenciadorTarefasAPI/Services/TarefaService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GerenciadorTarefasAPI.Models;
+using GerenciadorTarefasAPI.Repositories;
+
+namespace GerenciadorTarefasAPI.Services
+{
+    public class TarefaService
+    {
+        private readonly ITarefaRepository _tarefaRepository;
+
+        public TarefaService(ITarefaRepository tarefaRepository)
+        {
+            _tarefaRepository = tarefaRepository;
+        }
+
+        public async Task<IEnumerable<Tarefa>> GetTarefasByProjetoIdAsync(int projetoId)
+        {
+            return await _tarefaRepository.GetTarefasByProjetoIdAsync(projetoId);
+        }
+
+        public async Task CreateTarefaAsync(int projetoId, Tarefa tarefa)
+        {
+            if ((await _tarefaRepository.GetTarefasByProjetoIdAsync(projetoId)).Count() >= 20)
+            {
+                throw new InvalidOperationException("Não pode adicionar mais tarefas neste projeto.");
+            }
+
+            tarefa.CriadoEm = DateTime.UtcNow;
+            tarefa.ProjetoId = projetoId;
+            await _tarefaRepository.AddTarefaAsync(tarefa);
+        }
+
+        public async Task UpdateTarefaAsync(int tarefaId, Tarefa tarefa)
+        {
+            var tarefaExistente = await _tarefaRepository.GetTarefaByIdAsync(tarefaId);
+            if (tarefaExistente == null)
+            {
+                throw new InvalidOperationException("Tarefa não encontrada.");
+            }
+
+            if (tarefaExistente.Prioridade != tarefa.Prioridade)
+            {
+                throw new InvalidOperationException("Não é permitido alterar a prioridade da tarefa.");
+            }
+
+            tarefaExistente.Descricao = tarefa.Descricao;
+            tarefaExistente.Status = tarefa.Status;
+            tarefaExistente.AtualizadoEm = DateTime.UtcNow;
+
+            var historico = new TarefaHistorico
+            {
+                TarefaId = tarefaId,
+                AlteraDescricao = "Tarefa atualizada",
+                AlteraDate = DateTime.UtcNow,
+                AlteradoPor = "Usuário" // Deve ser substituído pelo usuário real em um aplicativo real
+            };
+
+            await _tarefaRepository.AddTaskHistoryAsync(historico);
+            await _tarefaRepository.UpdateTarefaAsync(tarefaExistente);
+        }
+
+        public async Task DeleteTarefaAsync(int tarefaId)
+        {
+            await _tarefaRepository.RemoveTarefaAsync(tarefaId);
+        }
+    }
+}
