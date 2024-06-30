252137aa394e68cb802a56824b230ba1e8dab330
Marcio de Andrade <marcio@requestdev.net>
2024-06-30 14:01:48 -0400

    Controllers

---
+++ GerenciadorTarefasAPI/Controllers/ProjetoController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GerenciadorTarefasAPI.Models;
+using GerenciadorTarefasAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorTarefasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjetoController : ControllerBase
+    {
+        private readonly ProjetoService _projetoService;
+
+        public ProjetoController(ProjetoService projetoService)
+        {
+            _projetoService = projetoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Projeto>>> GetProjetos()
+        {
+            return Ok(await _projetoService.GetProjetosAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateProjeto(Projeto projeto)
+        {
+            await _projetoService.CreateProjetoAsync(projeto);
+            return CreatedAtAction(nameof(GetProjetos), new { id = projeto.Id }, projeto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> RemoveProjeto(int id)
+        {
+            try
+            {
+                await _projetoService.RemoveProjetoAsync(id);
+                return NoContent();
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ GerenciadorTarefasAPI/Controllers/TarefaController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GerenciadorTarefasAPI.Models;
+using GerenciadorTarefasAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorTarefasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefaController : ControllerBase
+    {
+        private readonly TarefaService _tarefaService;
+
+        public TarefaController(TarefaService tarefaService)
+        {
+            _tarefaService = tarefaService;
+        }
+
+        [HttpGet("projeto/{projetoId}")]
+        public async Task<ActionResult<IEnumerable<Tarefa>>> GetTarefasByProjetoId(int projetoId)
+        {
+            return Ok(await _tarefaService.GetTarefasByProjetoIdAsync(projetoId));
+        }
+
+        [HttpPost("projeto/{projetoId}")]
+        public async Task<ActionResult> CreateTarefa(int projetoId, Tarefa tarefa)
+        {
+            await _tarefaService.CreateTarefaAsync(projetoId, tarefa);
+            return CreatedAtAction(nameof(GetTarefasByProjetoId), new { projetoId = projetoId }, tarefa);
+        }
+
+        [HttpPut("{tarefaId}")]
+        public async Task<ActionResult> UpdateTarefa(int tarefaId, Tarefa tarefa)
+        {
+            try
+            {
+                await _tarefaService.UpdateTarefaAsync(tarefaId, tarefa);
+                return NoContent();
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{tarefaId}")]
+        public async Task<ActionResult> DeleteTarefa(int tarefaId)
+        {
+            await _tarefaService.DeleteTarefaAsync(tarefaId);
+            return NoContent();
+        }
+    }
+}
