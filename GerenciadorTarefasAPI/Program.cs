891410aa24a1a89b43ae9f12943080eb331d6786
Marcio de Andrade <marcio@requestdev.net>
2024-06-30 14:27:07 -0400

    Adicione o AutoMapper ao contêiner de serviços

--- GerenciadorTarefasAPI/Program.cs
+++ GerenciadorTarefasAPI/Program.cs
@@ -2,8 +2,21 @@ using Microsoft.EntityFrameworkCore;
 using GerenciadorTarefasAPI.Data;
 using GerenciadorTarefasAPI.Repositories;
 using GerenciadorTarefasAPI.Services;
+using Serilog;
+using AutoMapper;
+using GerenciadorTarefasAPI.Mappings;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar o Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -17,6 +30,8 @@ builder.Services.AddScoped<IProjetoRepository, ProjetoRepository>();
 builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
 builder.Services.AddScoped<ProjetoService>();
 builder.Services.AddScoped<TarefaService>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
