d9068ab60ebd424cf8779c53dd2279d6974ae7e7
Marcio de Andrade <marcio@requestdev.net>
2024-06-30 13:34:41 -0400

    Configuração DbContext

---
+++ GerenciadorTarefasAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GerenciadorTarefasAPI.Models;
+
+namespace GerenciadorTarefasAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<TarefaHistorico> TarefaHistoricos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tarefa>()
+                .HasOne(t => t.Projeto)
+                .WithMany(p => p.Tarefas)
+                .HasForeignKey(t => t.ProjetoId);
+
+            modelBuilder.Entity<TarefaHistorico>()
+                .HasOne(h => h.Tarefa)
+                .WithMany(t => t.Historicos)
+                .HasForeignKey(h => h.TarefaId);
+        }
+    }
+}
